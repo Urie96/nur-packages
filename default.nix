0277221b6ff1aa9bc7c50ca82f2482578bf1a79b
yangrui.0 <yangrui.0@bytedance.com>
2026-04-09 19:06:03 +0800

    test: ci

--- default.nix
+++ default.nix
@@ -17,16 +17,16 @@
   lib = import ./lib { inherit pkgs; }; # functions
   overlays = import ./overlays; # nixpkgs overlays
 
-  ncmdump = pkgs.callPackage ./pkgs/ncmdump { };
-  translate = pkgs.callPackage ./pkgs/translate { };
-  foxtrot = pkgs.callPackage ./pkgs/foxtrot { };
-  flatnvim = pkgs.callPackage ./pkgs/flatnvim { };
-  keywrap = pkgs.callPackage ./pkgs/keywrap { };
-  lazycmd = pkgs.callPackage ./pkgs/lazycmd { };
-  NeteaseCloudMusicApi = pkgs.callPackage ./pkgs/NeteaseCloudMusicApi { };
-  tencent-cloud-update-ssl = pkgs.callPackage ./pkgs/tencent-cloud-update-ssl { };
-  hackbook = pkgs.callPackage ./pkgs/hackbook { };
-  find-project-root = pkgs.callPackage ./pkgs/find-project-root { };
+  # ncmdump = pkgs.callPackage ./pkgs/ncmdump { };
+  # translate = pkgs.callPackage ./pkgs/translate { };
+  # foxtrot = pkgs.callPackage ./pkgs/foxtrot { };
+  # flatnvim = pkgs.callPackage ./pkgs/flatnvim { };
+  # keywrap = pkgs.callPackage ./pkgs/keywrap { };
+  # lazycmd = pkgs.callPackage ./pkgs/lazycmd { };
+  # NeteaseCloudMusicApi = pkgs.callPackage ./pkgs/NeteaseCloudMusicApi { };
+  # tencent-cloud-update-ssl = pkgs.callPackage ./pkgs/tencent-cloud-update-ssl { };
+  # hackbook = pkgs.callPackage ./pkgs/hackbook { };
+  # find-project-root = pkgs.callPackage ./pkgs/find-project-root { };
   sso = pkgs.callPackage ./pkgs/sso { };
-  love-yue = pkgs.callPackage ./pkgs/love-yue { };
+  # love-yue = pkgs.callPackage ./pkgs/love-yue { };
 }
