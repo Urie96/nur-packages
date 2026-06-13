87197bfd8ab890b2f531d1e46ad965f518970bb6
yangrui <lubui.com@gmail.com>
2026-06-13 19:04:17 +0800

    feat: up

--- pkgs/lazydeck/default.nix
+++ pkgs/lazydeck/default.nix
@@ -6,13 +6,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "lazydeck";
-  version = "0.1.0-unstable-2026-05-23";
+  version = "0.1.0-unstable-2026-06-13";
 
   src = fetchFromGitHub {
     owner = "Urie96";
     repo = pname;
-    rev = "8bd25e3ecd0a96be167656d64ce1cea231b483e0";
-    sha256 = "sha256-C2PDjsKIo/FPjr2Z2jxTlk2p3YTLv590USvEHliEUgQ=";
+    rev = "872b1b830a0ee2f6898f618374c6fe1db776a702";
+    sha256 = "sha256-ra3JFpnOnQhsI5gNIRl0wqK+y7TKiSu+YJ3k7ITHnXc=";
   };
 
   doCheck = false;
@@ -23,7 +23,7 @@ rustPlatform.buildRustPackage rec {
     openssl
   ];
 
-  cargoHash = "sha256-/510A1L9KjeAh0zlQX9CczMs8v8ZTMpVuKwIsFVGshE=";
+  cargoHash = "sha256-KnhbGT+kZRINFn6gi/qBuj3bI6UhP6c/AFSNXqrvRxI=";
 
   meta.mainProgram = "lazydeck";
 }
