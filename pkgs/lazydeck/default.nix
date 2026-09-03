36633202ddf2d1b055988f37b52b5e02eb0939cd
yangrui.0 <yangrui.0@bytedance.com>
2026-09-03 20:45:01 +0800

    feat: up

--- pkgs/lazydeck/default.nix
+++ pkgs/lazydeck/default.nix
@@ -6,13 +6,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "lazydeck";
-  version = "0.1.0-unstable-2026-09-01";
+  version = "0.1.0-unstable-2026-09-03";
 
   src = fetchFromGitHub {
     owner = "Urie96";
     repo = pname;
-    rev = "72f3f4af149668a213d326cb6412d104c353460e";
-    sha256 = "sha256-3/7hxTcB1rbC4SXpEO4zHXPyW7r6fm24ZJi3eD+GUZA=";
+    rev = "c48c1ea03b8f7857db71db7b6cf2d7996a2a6e5c";
+    sha256 = "sha256-kKiWwBNwdNGUEQzNRhJbmUYZbswbkW5VJLnvHmvSVC8=";
   };
 
   doCheck = false;
