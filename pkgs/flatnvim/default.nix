62c35e4f82da9c5c70ff95fee5dbd242668e7151
yangrui <lubui.com@gmail.com>
2026-09-17 00:43:40 +0800

    feat: up

--- pkgs/flatnvim/default.nix
+++ pkgs/flatnvim/default.nix
@@ -4,13 +4,13 @@
 }:
 buildGoModule rec {
   pname = "flatnvim";
-  version = "0.1.0-unstable-2026-07-22";
+  version = "0.1.0-unstable-2026-09-16";
 
   src = fetchFromGitHub {
     owner = "Urie96";
     repo = pname;
-    rev = "c249a1c03ab3c0a38ba1ecf26e99b3c20ebc1d32";
-    sha256 = "sha256-Jq36tgKpT2Kbu4afiirPGUL0B+aze5dMtiqYN+3htWI=";
+    rev = "dc4a69e07ae99162fda028e4b27c5d47ae35c826";
+    sha256 = "sha256-C/sX0BcNi/5w9GSYWheOUvgoeoGryD/8UMZKWmn9ERU=";
   };
 
   vendorHash = "sha256-/Bl4G5STa5lnNntZnMmt+BfES+N7ZYAwC9tzpuqUKcc=";
