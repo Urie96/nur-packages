ed029264032c05304cd2533f7cf8cd35abea333b
yangrui <lubui.com@gmail.com>
2026-09-24 09:42:32 +0800

    feat: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
   outputs =
     { self, nixpkgs }:
     let
